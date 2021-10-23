8a00a08258138246fe536f33b7b2e93cf9a0dbec
TatyanaDol <t.v.dol@yandex.ru>
2021-10-23 23:19:45 +0300

    Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import {constructPopupCard} from './popup-constructor.js';
+import {deactivateForm, activateForm} from './form-control.js';
 
 const mapCanvas = document.querySelector('.map__canvas');
 const popupCards = constructPopupCard(10);
 
 mapCanvas.appendChild(popupCards[0]);
 
+deactivateForm();
+activateForm();
